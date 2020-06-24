e6e6ec6a3206c10d836fcef6a60c1b6535f6b513
isabel k. lee <hello@isabelklee.co>
2020-06-24 12:52:37 -0400

    created monster form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const monstersURL = `http://localhost:3000/monsters`
 const monsterContainer = document.querySelector("#monster-container")
+const createMonsterContainer = document.querySelector("#create-monster")
 let pageNumber = 1
 
 fetchFiftyMonsters()
+createMonsterForm()
 
 function fetchFiftyMonsters() {
   fetch(`${monstersURL}/?_limit=50&_page=${pageNumber}`)
@@ -47,4 +49,23 @@ function nextPage() {
       nextPage()
     })
   })
+}
+
+function createMonsterForm() {
+  let nameInput = document.createElement("input")
+  nameInput.placeholder = "Name" 
+  nameInput.autocomplete = "off"
+
+  let ageInput = document.createElement("input")
+  ageInput.placeholder = "Age" 
+  ageInput.autocomplete = "off"
+
+  let descriptionInput = document.createElement("input")
+  descriptionInput.placeholder = "Description" 
+  descriptionInput.autocomplete = "off"
+
+  let submitButton = document.createElement("button")
+  submitButton.innerText = "Create Monster"
+
+  createMonsterContainer.append(nameInput, ageInput, descriptionInput, submitButton)
 }
