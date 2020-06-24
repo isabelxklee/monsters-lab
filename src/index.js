e3a5ce03910ab19e3f30bc7e809849d8388b3296
isabel k. lee <hello@isabelklee.co>
2020-06-24 12:47:31 -0400

    added pagination

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const monstersURL = `http://localhost:3000/monsters`
-const fiftyMonstersURL = `http://localhost:3000/monsters/?_limit=50&_page=1`
 const monsterContainer = document.querySelector("#monster-container")
+let pageNumber = 1
 
-fetch(fiftyMonstersURL)
-.then(r => r.json())
-.then((monsters) => {
-  monsters.forEach((monster) => {
-    renderMonster(monster)
+fetchFiftyMonsters()
+
+function fetchFiftyMonsters() {
+  fetch(`${monstersURL}/?_limit=50&_page=${pageNumber}`)
+  .then(r => r.json())
+  .then((monsters) => {
+    monsters.forEach((monster) => {
+      renderMonster(monster)
+    })
+    nextPage()
   })
-})
+}
 
 function renderMonster(monster) {
   let name = document.createElement("h2")
@@ -20,5 +25,26 @@ function renderMonster(monster) {
   let description = document.createElement("p")
   description.innerText = monster.description
 
-  monsterContainer.append(name, age, description)
+  let idNumber = document.createElement("p")
+  idNumber.innerText = `ID #${monster.id}`
+
+  monsterContainer.append(idNumber, name, age, description)
+}
+
+function nextPage() {
+  let nextButton = document.querySelector("#forward")
+  nextButton.addEventListener("click", (event) => {
+
+    pageNumber = pageNumber + 1
+    monsterContainer.innerHTML = ""
+
+    fetch(`${monstersURL}/?_limit=50&_page=${pageNumber}`)
+    .then(r => r.json())
+    .then((monsters) => {
+      monsters.forEach((monster) => {
+        renderMonster(monster)
+      })
+      nextPage()
+    })
+  })
 }
